308b964bea5dc55f4c2ce453d46cb00e38edfc28
Mark Rizko <mark@rizkocircle.com>
2024-02-18 12:42:06 -0800

    uploaded questions.json and moved some things around

---
+++ qna.h
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+#define MAX_C 100
+#define MAX_Q 100
+#define MAX_A 100
+
+typedef struct Answer {
+    char answer_test[MAX_A];
+    bool correct;
+} Answer;
+
+typedef struct Question {
+    char category[MAX_C];
+    char question[MAX_Q];
+    Answer answers[4];
+    int value;
+} Question;
